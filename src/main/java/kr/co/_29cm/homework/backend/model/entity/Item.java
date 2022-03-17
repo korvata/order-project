ccb160c8719a339dfe3bdb99f62fa25021362714
jihoon-alticast <jh.yoon@alticast.com>
2022-03-18 02:28:09 +0900

    JPA설정

--- src/main/java/kr/co/_29cm/homework/backend/model/entity/Item.java
+++ src/main/java/kr/co/_29cm/homework/backend/model/entity/Item.java
@@ -2,7 +2,9 @@ package kr.co._29cm.homework.backend.model.entity;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -13,8 +15,16 @@ import javax.persistence.Table;
 @Table(name = "ITEM")
 public class Item {
 
+    @Id
+    @Column(name = "ITEM_NO")
     private String itemNo;          //상품번호
+
+    @Column(name = "NAME")
     private String name;            //상품명
+
+    @Column(name = "PRICE")
     private int price;              //판매가격
+
+    @Column(name = "QUANTITY")
     private int quantity;           //재고수량
 }

dd6f5930d5d5f598f51a59f7c797da1932a5c2c6
jihoon-alticast <jh.yoon@alticast.com>
2022-03-17 13:39:40 +0900

    Initial commit

---
+++ src/main/java/kr/co/_29cm/homework/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package kr.co._29cm.homework.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/_29cm/homework/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co._29cm.homework.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
